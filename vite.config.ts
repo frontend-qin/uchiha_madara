19fa8409989fb38358d988f49644a262422f958e
Ben <samyuzhibo@gmail.com>
2023-04-03 19:45:52 +0800

    配置支持写 tsx 语法

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,45 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { fileURLToPath, URL } from "url";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx({
-      // options are passed on to @vue/babel-plugin-jsx
+    vueJsx(),
+    AutoImport({
+      imports: [
+        "vue",
+        "vue-router",
+        "pinia",
+        {
+          "naive-ui": [
+            "useDialog",
+            "useMessage",
+            "useNotification",
+            "useLoadingBar",
+          ],
+        },
+      ],
+      dts: "./src/types/auto-imports.d.ts",
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()],
+      dts: "./src/types/components.d.ts",
     }),
   ],
+  optimizeDeps: {
+    include: ["vue", "vue-router", "pinia"],
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
   server: {
     open: true,
     fs: {
